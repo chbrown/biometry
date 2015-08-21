8e476da2857e47527b85dd7bbc1f95137b18eb8c
Christopher Brown <io@henrian.com>
2015-08-21 14:58:09 -0500

    Obligatory type declarations

---
+++ type_declarations/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="DefinitelyTyped/async/async.d.ts" />
+/// <reference path="DefinitelyTyped/moment/moment-node.d.ts" />
+/// <reference path="DefinitelyTyped/react/react.d.ts" />
+/// <reference path="DefinitelyTyped/react-router/react-router.d.ts" />
+/// <reference path="DefinitelyTyped/redux/redux.d.ts" />
+
+/// <reference path="../node_modules/httprequest/httprequest.d.ts" />
