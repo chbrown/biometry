ae896675d2ac9a68aedff6a7c12d8a7040a6aa2f
Christopher Brown <io@henrian.com>
2015-10-24 13:14:24 -0500

    Wrong history module

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,11 +1,13 @@
 /// <reference path="DefinitelyTyped/async/async.d.ts" />
-/// <reference path="DefinitelyTyped/history/history.d.ts" />
 /// <reference path="DefinitelyTyped/moment/moment-node.d.ts" />
 /// <reference path="DefinitelyTyped/react/react.d.ts" />
 /// <reference path="DefinitelyTyped/react-router/react-router.d.ts" />
 /// <reference path="DefinitelyTyped/redux/redux.d.ts" />
 
 // lazy
+declare module "history" {
+  var createHistory: any;
+}
 declare module "redux-router" {
   var reduxReactRouter: any;
   var routerStateReducer: any;
