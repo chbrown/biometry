e3d0c639d052480e3cd5cc49fde08e5998e413f7
Christopher Brown <io@henrian.com>
2015-10-24 12:56:56 -0500

    Update type declarations; add history and redux-router shim

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,7 +1,12 @@
 /// <reference path="DefinitelyTyped/async/async.d.ts" />
+/// <reference path="DefinitelyTyped/history/history.d.ts" />
 /// <reference path="DefinitelyTyped/moment/moment-node.d.ts" />
 /// <reference path="DefinitelyTyped/react/react.d.ts" />
 /// <reference path="DefinitelyTyped/react-router/react-router.d.ts" />
 /// <reference path="DefinitelyTyped/redux/redux.d.ts" />
 
-/// <reference path="../node_modules/httprequest/httprequest.d.ts" />
+// lazy
+declare module "redux-router" {
+  var reduxReactRouter: any;
+  var routerStateReducer: any;
+}
