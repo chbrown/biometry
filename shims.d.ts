449b68f1c39fa684ea60af4041e1072fc116412c
Christopher Brown <io@henrian.com>
2015-11-30 13:02:56 -0600

    Latest react-router type declarations include history package

--- shims.d.ts
+++ shims.d.ts
@@ -1,8 +1,4 @@
 // being lazy
-declare module "history" {
-  var createHistory: any;
-  var createHashHistory: any;
-}
 declare module "redux-router" {
   var reduxReactRouter: any;
   var routerStateReducer: any;
