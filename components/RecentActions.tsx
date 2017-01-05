0e8913c072fd645f3dd33838ce1a1982c0af0d89
Christopher Brown <io@henrian.com>
2017-01-05 16:18:10 -0600

    Reformat RecentActions component as a table

--- components/RecentActions.tsx
+++ components/RecentActions.tsx
@@ -3,22 +3,24 @@ import * as moment from 'moment';
 import {connect} from 'react-redux';
 import {Action, Actiontype, GlobalState} from '../types';
 
-const ActionItem = ({action, actiontype, now, format = 'YYYY-MM-DD h:mma'}) => {
+const ActionRow = ({action, actiontype, now, format = 'YYYY-MM-DD h:mma'}) => {
   const instantaneous = (action.started - action.ended) === 0;
   const ago = moment(action.entered).from(now);
   return (
-    <div>
-      [<i title="entered">{ago}</i>]{' '}
-      <b>{actiontype ? actiontype.name : '(not found)'}</b>{': '}
-      {instantaneous ?
-        <span title="started/ended">{moment(action.started).format(format)}</span> :
-        <span>
-          <span title="started">{moment(action.started).format(format)}</span>
-          -
-          <span title="ended">{moment(action.ended).format(format)}</span>
-        </span>
-      }
-    </div>
+    <tr>
+      <td className="right">[<i title="entered">{ago}</i>]</td>
+      <td className="left padded"><b>{actiontype ? actiontype.name : '(not found)'}</b></td>
+      <td className="left">
+        {instantaneous ?
+          <span title="started/ended">{moment(action.started).format(format)}</span> :
+          <span>
+            <span title="started">{moment(action.started).format(format)}</span>
+            -
+            <span title="ended">{moment(action.ended).format(format)}</span>
+          </span>
+        }
+      </td>
+    </tr>
   );
 };
 
@@ -34,12 +36,12 @@ class RecentActions extends React.Component<RecentActionsProps, {}> {
   render() {
     const {actions, actiontypes, now, limit = 20} = this.props;
     return (
-      <div>
+      <table>
         {actions.slice(-limit).reverse().map(action =>
-          <ActionItem key={action.action_id} action={action} now={now}
+          <ActionRow key={action.action_id} action={action} now={now}
             actiontype={actiontypes.find(actiontype => actiontype.actiontype_id == action.actiontype_id)} />
         )}
-      </div>
+      </table>
     );
   }
 }
