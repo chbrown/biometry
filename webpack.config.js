e3c827d5b34543511d28efe32bdce8a806253b12
Christopher Brown <io@henrian.com>
2017-11-29 17:33:40 -0600

    Change var -> const in webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-var env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
