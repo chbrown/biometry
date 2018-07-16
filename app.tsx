0ec743c023113a62ff521135011df9380728b2d2
Christopher Brown <io@henrian.com>
2018-07-15 19:05:08 -0500

    Refactor main App component into separate module

--- app.tsx
+++ app.tsx
@@ -1,24 +1,15 @@
-import * as moment from 'moment'
 import * as React from 'react'
 import {render} from 'react-dom'
 import {combineReducers, createStore} from 'redux'
-import {Provider, connect} from 'react-redux'
-
-import MetricsTable from './components/MetricsTable'
-import RecentActions from './components/RecentActions'
-import ConfigNumber from './components/ConfigNumber'
-import ConfigCheckbox from './components/ConfigCheckbox'
+import {Provider} from 'react-redux'
 
 import * as reducers from './reducers'
-import {OperationType, Configuration, GlobalState} from './types'
-
-const packageDefinition = require('./package.json')
+import {OperationType} from './types'
+import App from './components/App'
 
 import './site.less'
 
-const buildTimestamp = __WEBPACK_TIMESTAMP__
-
-const reducer = combineReducers(reducers as any)
+const reducer = combineReducers(reducers)
 const store = createStore(reducer)
 
 document.addEventListener('visibilitychange', () => {
@@ -33,49 +24,8 @@ document.addEventListener('visibilitychange', () => {
   }
 })
 
-interface AppProps {
-  configuration: Configuration
-  now: Date
-}
-
-class App extends React.Component<AppProps> {
-  render() {
-    const {configuration, now} = this.props
-    const {daysPast} = configuration
-    const end = moment().endOf('day')
-    const start = end.clone().subtract(daysPast, 'd')
-    return (
-      <main>
-        <div className="flex">
-          <h2>Biometry App</h2>
-          <div>
-            <ConfigNumber label="Days Past" name="daysPast" />
-            <ConfigCheckbox label="Sort Alphabetically" name="sortAlphabetically" />
-            <ConfigCheckbox label="Exclude Empty" name="excludeEmpty" />
-          </div>
-        </div>
-        <section>
-          <MetricsTable start={start} end={end} />
-        </section>
-        <section>
-          <h3>Recent Additions</h3>
-          <RecentActions limit={20} />
-        </section>
-        <footer>
-          <div className="important">Now: {now.toISOString()}</div>
-          <div>Built: {buildTimestamp}</div>
-          <div>Version: {packageDefinition.version}</div>
-        </footer>
-      </main>
-    )
-  }
-}
-
-const mapStateToProps = ({configuration, now}: GlobalState) => ({configuration, now})
-const ConnectedApp = connect(mapStateToProps)(App)
-
 render((
   <Provider store={store}>
-    <ConnectedApp />
+    <App />
   </Provider>
 ), document.getElementById('app'))
---
+++ components/App.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import * as moment from 'moment'
+import {connect} from 'react-redux'
+
+import {Configuration, GlobalState} from '../types'
+
+import MetricsTable from './MetricsTable'
+import RecentActions from './RecentActions'
+import ConfigNumber from './ConfigNumber'
+import ConfigCheckbox from './ConfigCheckbox'
+
+const packageDefinition = require('../package.json')
+
+// webpack replaces this symbol when building the full bundle
+const buildTimestamp = __WEBPACK_TIMESTAMP__
+
+interface AppProps {
+  configuration: Configuration
+  now: Date
+}
+
+class App extends React.Component<AppProps> {
+  render() {
+    const {configuration, now} = this.props
+    const {daysPast} = configuration
+    const end = moment().endOf('day')
+    const start = end.clone().subtract(daysPast, 'd')
+    return (
+      <main>
+        <div className="flex">
+          <h2>Biometry App</h2>
+          <div>
+            <ConfigNumber label="Days Past" name="daysPast" />
+            <ConfigCheckbox label="Sort Alphabetically" name="sortAlphabetically" />
+            <ConfigCheckbox label="Exclude Empty" name="excludeEmpty" />
+          </div>
+        </div>
+        <section>
+          <MetricsTable start={start} end={end} />
+        </section>
+        <section>
+          <h3>Recent Additions</h3>
+          <RecentActions limit={20} />
+        </section>
+        <footer>
+          <div className="important">Now: {now.toISOString()}</div>
+          <div>Built: {buildTimestamp}</div>
+          <div>Version: {packageDefinition.version}</div>
+        </footer>
+      </main>
+    )
+  }
+}
+
+const mapStateToProps = ({configuration, now}: GlobalState) => ({configuration, now})
+const ConnectedApp = connect(mapStateToProps)(App)
+
+export default ConnectedApp
